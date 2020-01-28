08c09c33eb9f0835e8bd0f8698e7f8b0ef294e25
divimes <con_selazx@hotmail.com>
2020-01-28 13:55:25 -0600

    correcion de prueba 2

--- app/src/main/java/com/example/b4learning/MainActivity.java
+++ app/src/main/java/com/example/b4learning/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///chaleeeeeeeeeeeeeeeeeeeeeeeee
+        ///chaleeeeeeeeeeeeeeeeeeeeeeeeedfdfsd
         et1= (EditText) findViewById(R.id.etusuario);
         et2= (EditText) findViewById(R.id.etcontrasena);
     }
